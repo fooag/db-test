491b2bec5bddcf10d24bbdba980ae638705607a4
Kurt Junker <kurt.junker@foo.ag>
2022-01-20 13:17:14 +0100

    Init docker-compose.yml liquibase 4.7 postgres 13.5 changeset bin file für liquibase

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Datenbank-Test
+
+## Initialisieren
+
+- docker installieren (inkl. docker-compose)
+- Container hochfahren
+
+````shell
+docker-compose up -d
+````
+
+- changesets ausführen
+
+```shell
+./liquibase update
+```
+
+## Testaufgabe
+tbd
---
+++ db/hinz_vermittler.sql
@@ -0,0 +1,10 @@
+INSERT INTO import_hinz_vermittler (kuerzel, vorgesetzter, name, vorname, geburtsdatum, plz, ort, strasse) VALUES
+('sk', 'ck', 'Krause', 'Sophie', '1978-03-23', '36251', 'Chemnitz', 'Rosenweg 87'),
+('lh', 'ck', 'Hartmann', 'Lothar', '1963-01-20', '36251', 'Duisburg', 'Bachstraße 16'),
+('rz', 'ck', 'Ziegler', 'Rainer', '1977-11-02', '36304', 'Rostock', 'Erlenweg 90'),
+('wl', 'em', 'Lang', 'Waldemar', '1999-10-07', '96317', 'Leverkusen', 'Tannenweg 100'),
+('ck', 'em', 'König', 'Christa', '1979-08-19', '94544', 'Kassel', 'Bergstraße 27'),
+('as', 'wl', 'Schulz', 'Andre', '1971-12-15', '96317', 'Oberhausen', 'Waldweg 89'),
+('jh', 'em', 'Hofmann', 'Jörn', '1978-08-30', '96364', 'Dresden', 'Dorfstraße 13'),
+('gs', 'wl', 'Stein', 'Gerhard', '1977-01-14', '86668', 'Bonn', 'Kiefernweg 20'),
+('em', NULL, 'Mann', 'Erika', '1982-04-06', '96317', 'Recklinghausen', 'Rathausstraße 66 59');
